f51ab723674cfc2ad6c0a80fb92bcf251612638f
rodrigobsi <rodrigomendes.bsi@gmail.com>
2021-12-08 00:55:53 -0300

    Rotina 1.0

---
+++ rotina.js
@@ -0,0 +1,13 @@
+function $(id){
+        return document.getElementById(id);
+}
+window.onload=function(){
+         window.location="https://github.com/rodrigobsi?tab=repositories";             
+}
+
+/*
+function redirecionar(){
+        window.open('https://www.google.com/?gws_rd=ssl');
+        window.location.href = "https://web.dio.me/home";
+}
+*/
