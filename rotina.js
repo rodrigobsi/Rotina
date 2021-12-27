08a15d23baa0923bc3c9849799d6c8936f6152a7
rodrigobsi <rodrigomendes.bsi@gmail.com>
2021-12-27 00:48:15 -0300

    Atualização do código 2.0

--- rotina.js
+++ rotina.js
@@ -1,13 +1,15 @@
-function $(id){
+function $(id) {
+  
         return document.getElementById(id);
 }
-window.onload=function(){
-         window.location="https://github.com/rodrigobsi?tab=repositories";             
+window.onload = function () {
+        window.location = "https://github.com/rodrigobsi?tab=repositories";
 }
 
+ //abre na mesma página
 /*
 function redirecionar(){
-        window.open('https://www.google.com/?gws_rd=ssl');
-        window.location.href = "https://web.dio.me/home";
+        window.open('https://www.google.com/?gws_rd=ssl'); //Abre a página em outra aba. 
+        window.location.href = "https://web.dio.me/home"; //abre na mesma página
 }
 */
